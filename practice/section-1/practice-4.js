0f6cc7546f6f7281f3705505f8690e7067016848
Chaowei Zeng <sli616@163.com>
2019-07-09 10:32:06 +0800

    finish practice one.

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,7 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  return collectionA
+    .map(object => object.key)
+    .filter(key => objectB.value.includes(key));
 }
